adf8b35766cd9aa0833f6dd58f24fe9297c13e69
Sar-Hal <sar656hal@gmail.com>
2025-12-09 21:20:37 +0530

    ok

--- Lab_1_03122025/Q1.py
+++ Lab_1_03122025/Q1.py
@@ -1,4 +1,4 @@
 a = int(input("Enter first integer: "))
 b = int(input("Enter second integer: "))
 sum_ab = a + b
-print("The sum of", a, "and", b, "is:", sum_ab)
+print("The sum of", a, "and", b, "is:", a+b)
